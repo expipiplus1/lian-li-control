d722ff15b0d654beb62cc11171db3425a0b0f42d
Ellie Hermaszewska <git@monoid.al>
2023-10-02 01:11:21 +0800

    update module

--- flake.nix
+++ flake.nix
@@ -5,78 +5,106 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+    let
+      d = flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
 
-        unextendedHaskellPackages = pkgs.haskell.packages.ghc94;
-        haskellPackages = unextendedHaskellPackages.extend (self: super:
-          with pkgs.haskell.lib.compose;
-          (pkgs.lib.mapAttrs (n: markUnbroken) {
-            bindings-libusb = addBuildDepends [ pkgs.pkg-config pkgs.libusb1 ]
-              super.bindings-libusb;
-            hid = doJailbreak super.hid;
-          }));
+          unextendedHaskellPackages = pkgs.haskell.packages.ghc94;
+          haskellPackages = unextendedHaskellPackages.extend (self: super:
+            with pkgs.haskell.lib.compose;
+            (pkgs.lib.mapAttrs (n: markUnbroken) {
+              bindings-libusb = addBuildDepends [ pkgs.pkg-config pkgs.libusb1 ]
+                super.bindings-libusb;
+              hid = doJailbreak super.hid;
+            }));
 
-        packageName = "lian-li-pump-control";
-      in rec {
-        packages.${packageName} = with pkgs.haskell.lib.compose;
-          overrideCabal (d: ({
-            postInstall = ''
-              mkdir -p $out/lib/udev/rules.d/
-              cp ${self}/*.rules $out/lib/udev/rules.d/
-            '';
-          })) (haskellPackages.callCabal2nix packageName self { });
-        packages.default = with pkgs.haskell.lib.compose;
-          (haskellPackages.generateOptparseApplicativeCompletions
-            [ packageName ] packages.${packageName});
+          packageName = "lian-li-pump-control";
+        in rec {
+          packages.${packageName} = with pkgs.haskell.lib.compose;
+            overrideCabal (d: ({
+              postInstall = ''
+                mkdir -p $out/lib/udev/rules.d/
+                cp ${self}/*.rules $out/lib/udev/rules.d/
+              '';
+            })) (haskellPackages.callCabal2nix packageName self { });
+          packages.default = with pkgs.haskell.lib.compose;
+            (haskellPackages.generateOptparseApplicativeCompletions
+              [ packageName ] packages.${packageName});
 
-        devShell = haskellPackages.shellFor {
-          packages = p: [ packages.${packageName} ];
-          withHoogle = true;
-          buildInputs = with unextendedHaskellPackages; [
-            # hls puts an old fourmolu in scope :/, so make sure a desired one
-            # gets in there first
-            # fourmolu_0_13_1_0
-            haskell-language-server
-            cabal-install
-          ];
-        };
+          devShell = haskellPackages.shellFor {
+            packages = p: [ packages.${packageName} ];
+            withHoogle = true;
+            buildInputs = with unextendedHaskellPackages; [
+              # hls puts an old fourmolu in scope :/, so make sure a desired one
+              # gets in there first
+              # fourmolu_0_13_1_0
+              haskell-language-server
+              cabal-install
+            ];
+          };
+        });
+    in d // {
+      nixosModules.default = { lib, config, system, pkgs, ... }:
+        with lib;
+        let cfg = config.services.hardware.lian-li-pump-control;
+        in {
+          options.services.hardware.lian-li-pump-control = {
+            enable = mkEnableOption (mdDoc "Set Lian-Li pump settings");
 
-        nixosModules.default = { lib, config }:
-          with lib;
-          let cfg = config.services.hardware.lian-li-pump-control;
-          in {
-            options.services.hardware.lian-li-pump-control = {
-              enable = mkEnableOption (mdDoc "Set Lian-Li pump settings");
-
-              package = mkOption {
-                type = types.package;
-                default = packages.default;
-                description =
-                  mdDoc "Set version of lian-li-pump-control package to use.";
-              };
+            package = mkOption {
+              type = types.package;
+              default = d.packages.${pkgs.stdenv.hostPlatform.system}.default;
+              description =
+                mdDoc "Set version of lian-li-pump-control package to use.";
             };
-            config = mkIf cfg.enable {
-              environment.systemPackages = [ cfg.package ];
-              services.udev.packages = [ cfg.package ];
 
-              systemd.services.openrgb = {
-                description = "OpenRGB server daemon";
-                wantedBy = [
-                  "multi-user.target"
-                  "suspend.target"
-                  "hibernate.target"
-                  "hybrid-sleep.target"
-                  "suspend-then-hibernate.target"
-                ];
-                serviceConfig = {
-                  Type = "oneshot";
-                  ExecStart =
-                    "${cfg.package}/bin/lian-li-pump-control --get-speed";
-                };
+            speed = mkOption {
+              type = types.nullOr (types.either (types.enum [ "pwm" ])
+                (types.ints.between 2200 4200));
+              default = null;
+              description =
+                mdDoc ''Desired pump RPM, or "pwm" to track the pwm input'';
+            };
+
+            color = mkOption {
+              type = types.nullOr (types.either (types.enum [ "sync" ])
+                (types.strMatching "[0-9a-fA-F]{6}:[0-9a-fA-F]{6}"));
+              default = null;
+              description = mdDoc ''
+                Desired pump inner and outer color (rrggbb:rrggbb), or "sync" to track the rgb input'';
+            };
+          };
+          config = mkIf cfg.enable {
+            environment.systemPackages = [ cfg.package ];
+            services.udev.packages = [ cfg.package ];
+
+            systemd.services.lian-li-pump-control = {
+              description = "Set lian-li pump settings";
+              wantedBy = [
+                "multi-user.target"
+                "suspend.target"
+                "hibernate.target"
+                "hybrid-sleep.target"
+                "suspend-then-hibernate.target"
+              ];
+              serviceConfig = {
+                Type = "oneshot";
+                ExecStart =
+                  "${cfg.package}/bin/lian-li-pump-control --get-speed ${
+                    if cfg.speed == null then
+                      ""
+                    else
+                      "--set-speed ${builtins.toString cfg.speed}"
+                  } ${
+                    if cfg.color == null then
+                      ""
+                    else
+                      "--set-color ${builtins.toString cfg.color}"
+                  }";
               };
             };
           };
-      });
+        };
+    };
 }
